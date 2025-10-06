01b271f0d8c0934c62be12ed4ead840580a08be5
Akshay <90514084+crakshay1@users.noreply.github.com>
2025-10-06 14:44:32 +0000

    Update demo.py

--- 6-Pyruvate-Valorization/demo.py
+++ 6-Pyruvate-Valorization/demo.py
@@ -1,9 +1,10 @@
-import find_best_pw_for_compound as bpw
+import search as bpw
 
 if __name__ == "__main__":
     map = "map00720" # Other carbon fixation pathways
     compound = "C00022" # Pyruvate
     maps = bpw.find_maps_from_compound(compound)
+    # We can do it for every map having the given compound
     #for element in maps:
          #bpw.visualize_best_subpathway(map, compound) 
     bpw.visualize_best_subpathway(map, compound) 
